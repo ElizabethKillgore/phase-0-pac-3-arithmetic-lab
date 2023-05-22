1b16cb1600a421f2ee10bcfab19a6ff0aada8a21
Elizabeth <ek@elizabethkillgore.com>
2023-05-22 14:06:24 -0500

    push EK

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(value1, value2) {
+return `value1 + value2 = value3`;   
+}
+
+function subtract(value1, value2) {
+    return `value - value2 = value3`;
+}
+
+function multiply(value1, value2) {
+    return `value1 * value2 = value3`;
+}
+
+function divide(value1, value2) {
+    return `value1 / value2 = value3`;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+ 
+function increment(n) {
+    return n+1;    
+}
+
+function decrement(n) {
+    return n-1;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+
+
